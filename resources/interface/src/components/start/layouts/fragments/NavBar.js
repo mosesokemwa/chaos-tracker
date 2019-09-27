cdeeb0ad0978b44abc87474f42f3e00f074291bc
Moses Okemwa <okemwamoses@gmail.com>
2019-09-27 15:35:54 +0300

    navigation bar

--- resources/interface/src/components/start/layouts/fragments/NavBar.js
+++ resources/interface/src/components/start/layouts/fragments/NavBar.js
@@ -1,33 +1,43 @@
 import * as React from "react";
 import {Link, NavLink, Route} from "react-router-dom";
-import {connect} from "react-redux";
-import {faLock} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 class NavBar extends React.Component{
     render() {
         return (
             <div>
                 <nav className='navbar navbar-light shadow-sm bg-light navbar-expand-sm'>
-                    <Route path='/grading' render={()=>(
+                    <Route path='/logs' render={()=>(
                         <>
-                            <Link to="/grading/" className='navbar-brand'>
-                                Ip Manager
+                            <Link to="/logs" className='navbar-brand'>
+                                Logs Manager
                             </Link>
                             <ul className='navbar-nav ml-auto nav-pills'>
                                 <li className='nav-item mx-1'>
-                                    <NavLink className='btn  btn-outline-secondary' to="/grading/history" >
-                                        History
+                                    <NavLink className='btn  btn-outline-secondary' to="/logs" >
+                                        Logs
+                                    </NavLink>
+                                    <NavLink className='btn  btn-outline-secondary' to="/reports" >
+                                        Reports
                                     </NavLink>
                                 </li>
                             </ul>
                         </>
                     )}/>
-                    <Route path='/attendance' render={()=>(
+                    <Route path='/reports' render={()=>(
                         <>
-                            <Link to="/attendance/" className='navbar-brand'>
-                                Attendance Manager
+                            <Link to="/reports" className='navbar-brand'>
+                                Reports Manager
                             </Link>
+                            <ul className='navbar-nav ml-auto nav-pills'>
+                                <li className='nav-item mx-1'>
+                                    <NavLink className='btn  btn-outline-secondary' to="/logs" >
+                                        Logs
+                                    </NavLink>
+                                    <NavLink className='btn  btn-outline-secondary' to="/reports" >
+                                        Reports
+                                    </NavLink>
+                                </li>
+                            </ul>
                         </>
                     )}/>
 
